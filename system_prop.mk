7bdba043d0ca7c3165213bda3517860fc31cc0bf
Sean Hoyt <seanhoyt963@gmail.com>
2016-02-23 04:23:30 -0600

    HACKS: Enable Force 4x MSAA and disable
    persistent GPU compositing
    
    GPU compositing needs this commit:
    http://review.cyanogenmod.org/#/c/132998/
    
    Change-Id: I46bb8fedfb09c1da090f949a305a86c193d9d08d

--- system_prop.mk
+++ system_prop.mk
@@ -17,3 +17,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.hsxpa=1 \
     ro.sec.fle.encryption=true \
     ro.secwvk=220
+
+# Fixes game grashing (HeartStone)
+debug.egl.force_msaa=true
+
+# Disable persistent GPU compositing
+debug.sf.disable_hwc=1
