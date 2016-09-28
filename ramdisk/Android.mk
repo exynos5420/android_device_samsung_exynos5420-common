d87ccf97ba0261b067241147df6f7243d24203af
deadman96385 <seanhoyt963@gmail.com>
2016-09-28 04:35:04 -0500

    Add usb and wifi scripts

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -15,3 +15,19 @@ LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= init.universal5420.power.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.universal5420.usb.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= init.universal5420.usb.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.universal5420.wifi.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= init.universal5420.wifi.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
