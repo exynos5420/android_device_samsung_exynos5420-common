ac7b1ad0ad0db16719636520324939fe848b3e8c
deadman96385 <seanhoyt963@gmail.com>
2016-09-28 04:05:21 -0500

    Build lights hal

--- exynos5420.mk
+++ exynos5420.mk
@@ -29,6 +29,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     consumerir.universal5420
 
+# Lights
+PRODUCT_PACKAGES += \
+    lights.universal5420
+
 # Media profile
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
