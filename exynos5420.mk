a618b18cd87a081a562b496e6b329f1b03d707a9
deadman96385 <seanhoyt963@gmail.com>
2016-09-28 04:04:43 -0500

    Add frameworks/av media codecs

--- exynos5420.mk
+++ exynos5420.mk
@@ -29,6 +29,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     consumerir.universal5420
 
+# Media profile
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
 # MobiCore setup
 PRODUCT_PACKAGES += \
     libMcClient \
