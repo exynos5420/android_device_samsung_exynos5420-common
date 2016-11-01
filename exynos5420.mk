43af14b574202e2783b8c4182710e8623133e92c
deadman96385 <seanhoyt963@gmail.com>
2016-11-01 18:37:55 -0500

    Force screenshot path to cpu consumer

--- exynos5420.mk
+++ exynos5420.mk
@@ -36,6 +36,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     RemovePackages
 
+# GPU producer to CPU consumer not supported
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bq.gpu_to_cpu_unsupported=1
+
 # call Samsung LSI board support package
 $(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
 $(call inherit-product, hardware/samsung_slsi-cm/exynos5420/exynos5420.mk)
