be923ef26174449a20278ace2241184c60feaee8
deadman96385 <seanhoyt963@gmail.com>
2016-11-02 00:38:12 -0500

    Add audio configs
    mixer_path stays in the device tree they are all diffrent

--- exynos5420.mk
+++ exynos5420.mk
@@ -32,6 +32,10 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     tinymix
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
+
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
 
