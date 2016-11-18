1ffad8e5f26dfedf3bcda7b7e379e63707de20ed
tincho5588 <tincho5588@gmail.com>
2016-11-18 20:24:31 -0300

    Update dalvik/hwui for 3GB
    we have 3GB of ram, why wont we do it¿
    
    Change-Id: I2c750ef3b340fcbea370bc8cfa8c17e0cabe5859

--- exynos5420.mk
+++ exynos5420.mk
@@ -181,8 +181,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.debug.multi_window=true
 
-# call dalvik heap config
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+# call dalvik heap and hwui config
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # call Samsung LSI board support package
 $(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
