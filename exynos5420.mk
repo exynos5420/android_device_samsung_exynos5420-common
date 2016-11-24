86b9f206aca4eea9c1335a79d5de72996916239e
Sean Hoyt <seanhoyt963@gmail.com>
2016-11-24 12:59:24 -0600

    device.mk: stagefright: Declare legacy and less-secure media
    This enables hardware video decoding for our device.
    
    Change-Id: I1a32d97416b13c79358276cfc72056b301b0cfca

--- exynos5420.mk
+++ exynos5420.mk
@@ -186,6 +186,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.debug.multi_window=true
 
+# Legacy stagefright media
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
+
 # call dalvik heap and hwui config
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 
