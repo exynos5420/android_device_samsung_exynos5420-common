e632e66e943982766bea14c7b6073607f2ab3de6
tincho5588 <tincho5588@gmail.com>
2016-12-03 04:06:39 -0300

    Added Deathly Adiutor presigned apk
    Free App apk with ads
    
    App version is 25
    
    Change-Id: I810993e4523430ca8f3ef000d5d2d5f686c2b599

--- exynos5420.mk
+++ exynos5420.mk
@@ -184,6 +184,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Enable multi-window by default
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.debug.multi_window=true
+	
+# Deathly Adiutor App
+PRODUCT_PACKAGES += \
+    DeathlyAdiutor
 
 # call dalvik heap and hwui config
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
