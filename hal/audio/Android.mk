a99e91f731a7fb6ac8e06ac194a035d93e9a6c4f
zakooz <marcotribosklol@gmail.com>
2016-12-25 15:12:21 +0000

    [Do not merge] Audio HAL from n1a
    Handles mic differently, let's see what happens
    
    Change-Id: Iea81f0ee001eb2d67686426a1fc2a35600d8c489

--- hal/audio/Android.mk
+++ hal/audio/Android.mk
@@ -12,28 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(TARGET_DEVICE),ha3g)
-
 LOCAL_PATH := $(call my-dir)
 
 # Audio HAL
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := audio.primary.$(TARGET_BOOTLOADER_BOARD_NAME)
-LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := audio_hw.c ril_interface.c
 
 LOCAL_C_INCLUDES += \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 	external/tinyalsa/include \
+	hardware/samsung_slsi/exynos5/include \
 	$(call include-path-for, audio-effects) \
 	$(call include-path-for, audio-utils) \
-	$(call include-path-for, audio-route)
+	$(call include-path-for, audio-route) \
+        $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 LOCAL_ADDITIONAL_DEPENDENCIES += \
 	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
 
 LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl \
 	libaudience_voicefx libaudioroute
@@ -59,5 +59,3 @@ LOCAL_SHARED_LIBRARIES := liblog libutils
 LOCAL_CFLAGS := -Wno-unused-parameter
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif
