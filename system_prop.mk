6a78359ffb1bfff8c9c43e37fb9bfa82168b9a5a
Martin Bouchet <tincho5588@gmail.com>
2017-02-09 02:59:22 -0300

    WIP: Move to a modular board vs target approach
    Change-Id: I26c635a0f5ea4da395f20c45a6169ab55c245f2e

--- system_prop.mk
+++ system_prop.mk
@@ -6,15 +6,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sapa.jack.master=alsa \
     ro.hdcp2.rx=tz
-    persist.bluetooth.disableabsvol=true
-
-# Open GL
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=196609
-
-# Disable persistent GPU compositing
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.disable_hwc=1
 
 # Randomly from stock
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -22,3 +13,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.hsxpa=1 \
     ro.sec.fle.encryption=true \
     ro.secwvk=220
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.dex2oat-swap=false
+
+# Enable multi-window by default
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.debug.multi_window=true
+
+# IO Scheduler
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.io.scheduler=bfq
+
+# Dex2oat optimizations
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.sys.fw.dex2oat_thread_count=4
