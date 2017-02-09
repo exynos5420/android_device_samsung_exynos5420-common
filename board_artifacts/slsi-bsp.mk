56f20f27f5253166147f11301bc43488ac13c39a
Martin Bouchet <tincho5588@gmail.com>
2017-02-09 02:59:22 -0300

    WIP: Move to a modular board vs target approach
    Change-Id: I26c635a0f5ea4da395f20c45a6169ab55c245f2e

---
+++ board_artifacts/slsi-bsp.mk
@@ -0,0 +1,3 @@
+# call Samsung LSI board support package
+$(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
+$(call inherit-product, hardware/samsung_slsi-cm/exynos5420/exynos5420.mk)
---
+++ board_defines/sec-mmc.mk
@@ -0,0 +1,2 @@
+# Disable journaling on system.img to save space
+BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
