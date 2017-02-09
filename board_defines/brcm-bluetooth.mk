d666eee2e2cb5b390673eb57566e470393b7d998
Martin Bouchet <tincho5588@gmail.com>
2017-02-09 02:59:22 -0300

    WIP: Move to a modular board vs target approach
    Change-Id: I26c635a0f5ea4da395f20c45a6169ab55c245f2e

---
+++ board_defines/brcm-bluetooth.mk
@@ -0,0 +1,7 @@
+DEVICE_PATH := device/samsung/exynos5420-common
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_HAVE_SAMSUNG_BLUETOOTH := true
+BOARD_CUSTOM_BT_CONFIG := $(DEVICE_PATH)/bluetooth/libbt_vndcfg.txt
