5a74296f1c13e13a57c2e6628686af75b64adc19
zakooz <marcotribosklol@gmail.com>
2017-08-29 11:40:43 +0100

    media: Remove ffmpeg codecs

--- exynos5420.mk
+++ exynos5420.mk
@@ -75,7 +75,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(COMMON_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
-    $(COMMON_PATH)/configs/media/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     $(COMMON_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
 
