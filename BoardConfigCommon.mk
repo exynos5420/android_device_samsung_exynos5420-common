a36f3430da8153752cc2ccda5ce648c614a4872e
Martin Bouchet <tincho5588@gmail.com>
2017-09-05 18:55:19 -0300

    BoardConfig: disable low power audio
    we can't make use of it anyway
    
    Change-Id: Ief2e69d1d524635dd31799b286daaad5f5a0f344

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,9 +150,6 @@ WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
 
-# Advanced Low Power audio support
-BOARD_USE_ALP_AUDIO := true
-
 # Linker
 LINKER_FORCED_SHIM_LIBS := /system/vendor/lib/egl/libGLES_mali.so|libpopcountsi2.so
 
