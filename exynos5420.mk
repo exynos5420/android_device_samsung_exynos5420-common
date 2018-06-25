51b9d97b207793d04e20abddc5ff42174db4c84a
Thomas Patry <thompatry@gmail.com>
2018-06-25 06:57:53 -0500

    exynos5420-common: Update GPS headers and add GNSS for O
    Change-Id: Ida235a780740847e5f50b1a66306252ef3c00904

--- exynos5420.mk
+++ exynos5420.mk
@@ -44,6 +44,10 @@ PRODUCT_PACKAGES += \
     camera.universal5420 \
     libhwjpeg
 
+#GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
 # Graphics
 PRODUCT_PACKAGES += \
     libion \
