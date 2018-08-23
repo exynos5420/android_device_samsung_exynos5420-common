85333ab36b955adc9efc51dbadcee5bd73a31a85
Thomas Patry <thompatry@gmail.com>
2018-08-23 18:23:20 -0400

    DO NOT MERGE:Camera bring up to Oreo
    Change-Id: I4bc2887a8312c20ad27d7af067e31acf8762de6d

--- exynos5420.mk
+++ exynos5420.mk
@@ -41,7 +41,11 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-service \
+    camera.device@1.0-impl-legacy \
     camera.universal5420 \
+    libshim_camera \
     libhwjpeg
 
 #GPS
