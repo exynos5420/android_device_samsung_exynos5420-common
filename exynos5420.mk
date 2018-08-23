52ecbcff44e3a0c524c85fd9259682de8aa53592
Thomas Patry <thompatry@gmail.com>
2018-08-23 18:23:20 -0400

    exynos5420-common: Camera bring up to Pie
    Change-Id: I4bc2887a8312c20ad27d7af067e31acf8762de6d

--- exynos5420.mk
+++ exynos5420.mk
@@ -42,7 +42,9 @@ TARGET_BOOTANIMATION_HALF_RES := true
 # Camera
 PRODUCT_PACKAGES += \
     camera.device@1.0-impl.exynos5420 \
+    android.hardware.camera.provider@2.4-impl.exynos5420 \
     camera.universal5420 \
+    libshim_camera \
     libhwjpeg
 
 #GPS
