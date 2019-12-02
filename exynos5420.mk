5bf6c80270e0496ee4d7261ec18c99060ffd32db
Reza Feizi <rezaf136@gmail.com>
2019-12-02 14:29:23 -0400

    Remove DeathlyAdiutor
    Change-Id: Ie00d34e4ed32205b754a21a70f3910f668df16ab

--- exynos5420.mk
+++ exynos5420.mk
@@ -200,10 +200,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.debug.multi_window=true
 	
-# Deathly Adiutor App
-PRODUCT_PACKAGES += \
-    DeathlyAdiutor
-
 # Legacy stagefright media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.legacyencoder=true \
