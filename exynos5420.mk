7ea33a2f6885efd806efee744fddd8793ca4f90b
Thomas Patry <thompatry@gmail.com>
2020-10-28 19:10:59 +0000

    exynos5420-common: hals: Import HIDL Camera HAL from hardware_lineage_interfaces.
    Change-Id: I3a30d4ed22e49680bdcdb4a36bc8c8bffdd5a7ce

--- exynos5420.mk
+++ exynos5420.mk
@@ -46,6 +46,8 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl.exynos5420 \
+    camera.device@1.0-impl.exynos5420 \
     camera.universal5420 \
     libhwjpeg
 
