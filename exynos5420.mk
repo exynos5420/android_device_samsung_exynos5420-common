e0f6ad4c6a344576c81bab2479642bc1e3b21559
Thomas Patry <thompatry@gmail.com>
2020-10-30 17:37:38 +0000

    exynos5420-common: Use Samsung light sevice
    Change-Id: Ie33aaed89bea3910d8ce1cf4f6d59f5c98e79b2c

--- exynos5420.mk
+++ exynos5420.mk
@@ -82,7 +82,7 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     lights.universal5420 \
-    android.hardware.light@2.0-impl
+    android.hardware.light@2.0-service.samsung
 
 # Media profile
 PRODUCT_COPY_FILES += \
