ad4554991ca07dedc64baa579a4870f9456506eb
Thomas Patry <thompatry@gmail.com>
2020-10-30 17:38:37 +0000

    exynos5420-common: Add Lineage livedisplay target
    Change-Id: Ib90cbd42b29b75b0bde4310f132bb135fa9a2dbf

--- exynos5420.mk
+++ exynos5420.mk
@@ -83,6 +83,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.samsung
 
+# Livedisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service.samsung-exynos
+
 # Media profile
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
