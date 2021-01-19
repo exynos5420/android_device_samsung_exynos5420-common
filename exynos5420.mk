8e22c89d215ffdbd652b15aaa2daefcee9ebbe26
Thomas Patry <thompatry@gmail.com>
2021-01-19 05:26:18 +0000

    exynos5420-common: build ramdisk target for recovery
    Change-Id: Icee766dae9729ffa3440c74e7fd68b69dee25fe8

--- exynos5420.mk
+++ exynos5420.mk
@@ -152,6 +152,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.samsung.rc \
     init.universal5420.rc \
+    init.recovery.universal5420.rc \
     init.universal5420.usb.rc \
     init.universal5420.wifi.rc
 
