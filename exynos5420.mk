e69c0d1366b5550a2acd48543c202c311a229baf
retiredtab-gerrit <clrh1tablet@gmail.com>
2021-02-12 17:28:24 -0700

    exynos5420-common: exynos5420.mk - add bluetooth hal entries
    Change-Id: Ie3f96578e34f087a60a0566fe56998e023a1ff06

--- exynos5420.mk
+++ exynos5420.mk
@@ -41,6 +41,11 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
     $(COMMON_PATH)/configs/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf
 
+ # Bluetooth HAL
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor
+
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
 
