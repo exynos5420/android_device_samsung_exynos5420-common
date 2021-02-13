ee3d1c6e036c016b47b25149825c3fcb6d8f205d
retiredtab-gerrit <clrh1tablet@gmail.com>
2021-02-12 17:21:30 -0700

    exynos5420-common: exynos5420.mk - bump up audio hal to 4.0
    Do not use android.hardware.audio@2.0-service as it causes stuck at boot animation.
    android.hardware.audio@2.0-impl works fine.
    
    Change-Id: Ia8b75f77cb277cd8c17f94cb7361e8a93ef3353e

--- exynos5420.mk
+++ exynos5420.mk
@@ -30,7 +30,11 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     tinymix \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio@4.0-implmsd \
+    android.hardware.audio.effect@4.0-impl
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
