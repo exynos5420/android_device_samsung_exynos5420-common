7406313170c66d22d13d2da2905a60fb54a1b9e8
retiredtab-gerrit <clrh1tablet@gmail.com>
2021-02-12 17:32:05 -0700

    exynos5420-common: exynos5420.mk - add DRM
    Change-Id: Ife466bb514766278489c68e0fa478dd30afdcbfc

--- exynos5420.mk
+++ exynos5420.mk
@@ -62,6 +62,12 @@ PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-impl \
     android.hardware.configstore@1.1-service
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.1-service.clearkey \
+    android.hardware.drm@1.0-service
+
 # First Stage Mount
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/ramdisk/fstab.universal5420:$(TARGET_COPY_OUT_RAMDISK)/fstab.universal5420 \
