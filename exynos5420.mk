4c926fadad6110f024feadbc4cc63d37f1a7e4c1
retiredtab-gerrit <clrh1tablet@gmail.com>
2021-02-12 17:46:53 -0700

    exynos5420-common: exynos5420.mk - remove android.hardware.vibrator@1.0-service, use android.hardware.vibrator@1.0-impl
    android.hardware.vibrator@1.0-service causes stuck at boot animation.
    
    Change-Id: Ic3688868f718ba8298b2239de6acabb101a0fbb5

--- exynos5420.mk
+++ exynos5420.mk
@@ -209,8 +209,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator HAL
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-impl
 
 # USB HAL
 PRODUCT_PACKAGES += \
