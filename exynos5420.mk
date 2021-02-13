66ffcae0f09da74e67121e598fa8a7c82fc2ee21
retiredtab-gerrit <clrh1tablet@gmail.com>
2021-02-12 17:35:52 -0700

    exynos5420-common: exynos5420.mk - add LiveDisplay
    Change-Id: I157d4a0555397c59f7120d8237efbf901fe136f7

--- exynos5420.mk
+++ exynos5420.mk
@@ -119,6 +119,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.samsung
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service.samsung-exynos
+
 # Media profile
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
